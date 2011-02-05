a5501aee4bd27314ad7c0de80bb966e806ba30ca
Evan Phoenix <ephoenix@engineyard.com>
2011-02-04 22:00:55 -0800

    Show proper syntax/parse error

--- bin/repl.rb
+++ bin/repl.rb
@@ -4,9 +4,15 @@ require 'pp'
 
 while str = Readline.readline("> ")
   break if str.empty?
+
   parser = Prattle::Parser.new(str)
-  nodes = parser.parse
-  pp nodes
+  begin
+    nodes = parser.parse
+  rescue Prattle::Parser::ParseError => e
+    puts "Syntax error detected:"
+    e.parser.show_error
+    next
+  end
 
   eval = Prattle::Evaluator.new(nodes, true)
   puts "=> #{eval.run.inspect}"
